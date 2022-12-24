446abcaf39b02b4ca8e79c8c1c7736c1ccd021c4
Dan Lynch <pyramation@gmail.com>
2022-12-23 20:52:12 -0800

    tx

--- src/codegen/cosmos/feegrant/v1beta1/tx.ts
+++ src/codegen/cosmos/feegrant/v1beta1/tx.ts
@@ -17,6 +17,10 @@ export interface MsgGrantAllowance {
 
   allowance?: (BasicAllowance & PeriodicAllowance & AllowedMsgAllowance & Any) | undefined;
 }
+export interface MsgGrantAllowanceProtoType {
+  typeUrl: "/cosmos.feegrant.v1beta1.MsgGrantAllowance";
+  value: Uint8Array;
+}
 /**
  * MsgGrantAllowance adds permission for Grantee to spend up to Allowance
  * of fees from the account of Granter.
@@ -32,6 +36,10 @@ export interface MsgGrantAllowanceAmino {
 
   allowance?: AnyAmino;
 }
+export interface MsgGrantAllowanceAminoType {
+  type: "cosmos-sdk/MsgGrantAllowance";
+  value: MsgGrantAllowanceAmino;
+}
 /**
  * MsgGrantAllowance adds permission for Grantee to spend up to Allowance
  * of fees from the account of Granter.
@@ -45,9 +53,17 @@ export interface MsgGrantAllowanceSDKType {
 /** MsgGrantAllowanceResponse defines the Msg/GrantAllowanceResponse response type. */
 
 export interface MsgGrantAllowanceResponse {}
+export interface MsgGrantAllowanceResponseProtoType {
+  typeUrl: "/cosmos.feegrant.v1beta1.MsgGrantAllowanceResponse";
+  value: Uint8Array;
+}
 /** MsgGrantAllowanceResponse defines the Msg/GrantAllowanceResponse response type. */
 
 export interface MsgGrantAllowanceResponseAmino {}
+export interface MsgGrantAllowanceResponseAminoType {
+  type: "cosmos-sdk/MsgGrantAllowanceResponse";
+  value: MsgGrantAllowanceResponseAmino;
+}
 /** MsgGrantAllowanceResponse defines the Msg/GrantAllowanceResponse response type. */
 
 export interface MsgGrantAllowanceResponseSDKType {}
@@ -60,6 +76,10 @@ export interface MsgRevokeAllowance {
 
   grantee: string;
 }
+export interface MsgRevokeAllowanceProtoType {
+  typeUrl: "/cosmos.feegrant.v1beta1.MsgRevokeAllowance";
+  value: Uint8Array;
+}
 /** MsgRevokeAllowance removes any existing Allowance from Granter to Grantee. */
 
 export interface MsgRevokeAllowanceAmino {
@@ -68,6 +88,10 @@ export interface MsgRevokeAllowanceAmino {
   /** grantee is the address of the user being granted an allowance of another user's funds. */
 
   grantee: string;
+}
+export interface MsgRevokeAllowanceAminoType {
+  type: "cosmos-sdk/MsgRevokeAllowance";
+  value: MsgRevokeAllowanceAmino;
 }
 /** MsgRevokeAllowance removes any existing Allowance from Granter to Grantee. */
 
@@ -78,9 +102,17 @@ export interface MsgRevokeAllowanceSDKType {
 /** MsgRevokeAllowanceResponse defines the Msg/RevokeAllowanceResponse response type. */
 
 export interface MsgRevokeAllowanceResponse {}
+export interface MsgRevokeAllowanceResponseProtoType {
+  typeUrl: "/cosmos.feegrant.v1beta1.MsgRevokeAllowanceResponse";
+  value: Uint8Array;
+}
 /** MsgRevokeAllowanceResponse defines the Msg/RevokeAllowanceResponse response type. */
 
 export interface MsgRevokeAllowanceResponseAmino {}
+export interface MsgRevokeAllowanceResponseAminoType {
+  type: "cosmos-sdk/MsgRevokeAllowanceResponse";
+  value: MsgRevokeAllowanceResponseAmino;
+}
 /** MsgRevokeAllowanceResponse defines the Msg/RevokeAllowanceResponse response type. */
 
 export interface MsgRevokeAllowanceResponseSDKType {}
