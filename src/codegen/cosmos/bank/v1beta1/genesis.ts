1cd131c8bc77402be86c0f4f906138427de02d86
Dan Lynch <pyramation@gmail.com>
2022-12-23 20:55:18 -0800

    codegen

--- src/codegen/cosmos/bank/v1beta1/genesis.ts
+++ src/codegen/cosmos/bank/v1beta1/genesis.ts
@@ -1,5 +1,5 @@
-import { Params, ParamsAmino, ParamsSDKType, Metadata, MetadataAmino, MetadataSDKType } from "./bank";
-import { Coin, CoinAmino, CoinSDKType } from "../../base/v1beta1/coin";
+import { Params, ParamsSDKType, Metadata, MetadataSDKType } from "./bank";
+import { Coin, CoinSDKType } from "../../base/v1beta1/coin";
 import * as _m0 from "protobufjs/minimal";
 import { isSet } from "../../../helpers";
 /** GenesisState defines the bank module's genesis state. */
@@ -20,32 +20,6 @@ export interface GenesisState {
 
   denomMetadata: Metadata[];
 }
-export interface GenesisStateProtoType {
-  typeUrl: "/cosmos.bank.v1beta1.GenesisState";
-  value: Uint8Array;
-}
-/** GenesisState defines the bank module's genesis state. */
-
-export interface GenesisStateAmino {
-  /** params defines all the paramaters of the module. */
-  params?: ParamsAmino;
-  /** balances is an array containing the balances of all the accounts. */
-
-  balances: BalanceAmino[];
-  /**
-   * supply represents the total supply. If it is left empty, then supply will be calculated based on the provided
-   * balances. Otherwise, it will be used to validate that the sum of the balances equals this amount.
-   */
-
-  supply: CoinAmino[];
-  /** denom_metadata defines the metadata of the differents coins. */
-
-  denom_metadata: MetadataAmino[];
-}
-export interface GenesisStateAminoType {
-  type: "cosmos-sdk/GenesisState";
-  value: GenesisStateAmino;
-}
 /** GenesisState defines the bank module's genesis state. */
 
 export interface GenesisStateSDKType {
@@ -65,26 +39,6 @@ export interface Balance {
   /** coins defines the different coins this balance holds. */
 
   coins: Coin[];
-}
-export interface BalanceProtoType {
-  typeUrl: "/cosmos.bank.v1beta1.Balance";
-  value: Uint8Array;
-}
-/**
- * Balance defines an account address and balance pair used in the bank module's
- * genesis state.
- */
-
-export interface BalanceAmino {
-  /** address is the address of the balance holder. */
-  address: string;
-  /** coins defines the different coins this balance holds. */
-
-  coins: CoinAmino[];
-}
-export interface BalanceAminoType {
-  type: "cosmos-sdk/Balance";
-  value: BalanceAmino;
 }
 /**
  * Balance defines an account address and balance pair used in the bank module's
@@ -201,40 +155,6 @@ export const GenesisState = {
     message.supply = object.supply?.map(e => Coin.fromPartial(e)) || [];
     message.denomMetadata = object.denomMetadata?.map(e => Metadata.fromPartial(e)) || [];
     return message;
-  },
-
-  fromAmino(object: GenesisStateAmino): GenesisState {
-    return {
-      params: object?.params ? Params.fromAmino(object.params) : undefined,
-      balances: Array.isArray(object?.balances) ? object.balances.map((e: any) => Balance.fromAmino(e)) : [],
-      supply: Array.isArray(object?.supply) ? object.supply.map((e: any) => Coin.fromAmino(e)) : [],
-      denomMetadata: Array.isArray(object?.denom_metadata) ? object.denom_metadata.map((e: any) => Metadata.fromAmino(e)) : []
-    };
-  },
-
-  toAmino(message: GenesisState): GenesisStateAmino {
-    const obj: any = {};
-    obj.params = message.params ? Params.toAmino(message.params) : undefined;
-
-    if (message.balances) {
-      obj.balances = message.balances.map(e => e ? Balance.toAmino(e) : undefined);
-    } else {
-      obj.balances = [];
-    }
-
-    if (message.supply) {
-      obj.supply = message.supply.map(e => e ? Coin.toAmino(e) : undefined);
-    } else {
-      obj.supply = [];
-    }
-
-    if (message.denomMetadata) {
-      obj.denom_metadata = message.denomMetadata.map(e => e ? Metadata.toAmino(e) : undefined);
-    } else {
-      obj.denom_metadata = [];
-    }
-
-    return obj;
   }
 
 };
@@ -310,26 +230,6 @@ export const Balance = {
     message.address = object.address ?? "";
     message.coins = object.coins?.map(e => Coin.fromPartial(e)) || [];
     return message;
-  },
-
-  fromAmino(object: BalanceAmino): Balance {
-    return {
-      address: object.address,
-      coins: Array.isArray(object?.coins) ? object.coins.map((e: any) => Coin.fromAmino(e)) : []
-    };
-  },
-
-  toAmino(message: Balance): BalanceAmino {
-    const obj: any = {};
-    obj.address = message.address;
-
-    if (message.coins) {
-      obj.coins = message.coins.map(e => e ? Coin.toAmino(e) : undefined);
-    } else {
-      obj.coins = [];
-    }
-
-    return obj;
   }
 
 };
