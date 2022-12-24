93968459ce12501e4e105a73800f0f70ae5cfda5
Dan Lynch <pyramation@gmail.com>
2022-12-23 20:55:17 -0800

    codegen

--- src/codegen/cosmos/app/v1alpha1/config.ts
+++ src/codegen/cosmos/app/v1alpha1/config.ts
@@ -1,4 +1,4 @@
-import { Any, AnyAmino, AnySDKType } from "../../../google/protobuf/any";
+import { Any, AnySDKType } from "../../../google/protobuf/any";
 import * as _m0 from "protobufjs/minimal";
 import { isSet } from "../../../helpers";
 /**
@@ -14,28 +14,6 @@ import { isSet } from "../../../helpers";
 export interface Config {
   /** modules are the module configurations for the app. */
   modules: ModuleConfig[];
-}
-export interface ConfigProtoType {
-  typeUrl: "/cosmos.app.v1alpha1.Config";
-  value: Uint8Array;
-}
-/**
- * Config represents the configuration for a Cosmos SDK ABCI app.
- * It is intended that all state machine logic including the version of
- * baseapp and tx handlers (and possibly even Tendermint) that an app needs
- * can be described in a config object. For compatibility, the framework should
- * allow a mixture of declarative and imperative app wiring, however, apps
- * that strive for the maximum ease of maintainability should be able to describe
- * their state machine with a config object alone.
- */
-
-export interface ConfigAmino {
-  /** modules are the module configurations for the app. */
-  modules: ModuleConfigAmino[];
-}
-export interface ConfigAminoType {
-  type: "cosmos-sdk/Config";
-  value: ConfigAmino;
 }
 /**
  * Config represents the configuration for a Cosmos SDK ABCI app.
@@ -72,37 +50,6 @@ export interface ModuleConfig {
    */
 
   config?: Any;
-}
-export interface ModuleConfigProtoType {
-  typeUrl: "/cosmos.app.v1alpha1.ModuleConfig";
-  value: Uint8Array;
-}
-/** ModuleConfig is a module configuration for an app. */
-
-export interface ModuleConfigAmino {
-  /**
-   * name is the unique name of the module within the app. It should be a name
-   * that persists between different versions of a module so that modules
-   * can be smoothly upgraded to new versions.
-   * 
-   * For example, for the module cosmos.bank.module.v1.Module, we may chose
-   * to simply name the module "bank" in the app. When we upgrade to
-   * cosmos.bank.module.v2.Module, the app-specific name "bank" stays the same
-   * and the framework knows that the v2 module should receive all the same state
-   * that the v1 module had. Note: modules should provide info on which versions
-   * they can migrate from in the ModuleDescriptor.can_migration_from field.
-   */
-  name: string;
-  /**
-   * config is the config object for the module. Module config messages should
-   * define a ModuleDescriptor using the cosmos.app.v1alpha1.is_module extension.
-   */
-
-  config?: AnyAmino;
-}
-export interface ModuleConfigAminoType {
-  type: "cosmos-sdk/ModuleConfig";
-  value: ModuleConfigAmino;
 }
 /** ModuleConfig is a module configuration for an app. */
 
@@ -170,24 +117,6 @@ export const Config = {
     const message = createBaseConfig();
     message.modules = object.modules?.map(e => ModuleConfig.fromPartial(e)) || [];
     return message;
-  },
-
-  fromAmino(object: ConfigAmino): Config {
-    return {
-      modules: Array.isArray(object?.modules) ? object.modules.map((e: any) => ModuleConfig.fromAmino(e)) : []
-    };
-  },
-
-  toAmino(message: Config): ConfigAmino {
-    const obj: any = {};
-
-    if (message.modules) {
-      obj.modules = message.modules.map(e => e ? ModuleConfig.toAmino(e) : undefined);
-    } else {
-      obj.modules = [];
-    }
-
-    return obj;
   }
 
 };
@@ -257,20 +186,6 @@ export const ModuleConfig = {
     message.name = object.name ?? "";
     message.config = object.config !== undefined && object.config !== null ? Any.fromPartial(object.config) : undefined;
     return message;
-  },
-
-  fromAmino(object: ModuleConfigAmino): ModuleConfig {
-    return {
-      name: object.name,
-      config: object?.config ? Any.fromAmino(object.config) : undefined
-    };
-  },
-
-  toAmino(message: ModuleConfig): ModuleConfigAmino {
-    const obj: any = {};
-    obj.name = message.name;
-    obj.config = message.config ? Any.toAmino(message.config) : undefined;
-    return obj;
   }
 
 };
