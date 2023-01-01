582c46d50d24b548b20f50dc764f5c99c457be55
Dan Lynch <pyramation@gmail.com>
2022-12-31 17:35:04 -0800

    tests

--- __tests__/amino.test.ts
+++ __tests__/amino.test.ts
@@ -1,13 +1,14 @@
 import { cosmos } from '../src';
 import Long from 'long';
-import { MsgCreateGroupWithPolicy, DecisionPolicy_FromAmino } from '../src/codegen/cosmos/group/v1/tx';
-import { DecisionPolicy_ToAmino } from '../src/codegen/cosmos/group/v1/types';
+import { MsgCreateGroupWithPolicy, MsgCreateGroupWithPolicyEncoded, MsgCreateGroupWithPolicyProtoMsg, DecisionPolicy_FromAmino } from '../src/codegen/cosmos/group/v1/tx';
+import { DecisionPolicyWindows, DecisionPolicy_ToAmino, PercentageDecisionPolicyAmino } from '../src/codegen/cosmos/group/v1/types';
+import { Msg } from '../src/codegen/cosmos/authz/v1beta1/tx.rpc.msg';
 
 const { createGroupPolicy, createGroupWithPolicy } =
     cosmos.group.v1.MessageComposer.withTypeUrl;
 const { PercentageDecisionPolicy } = cosmos.group.v1;
 
-const msg = createGroupWithPolicy({
+const msg1 = createGroupWithPolicy({
     admin: 'address',
     groupMetadata: 'https://some.ipfs.w3s.link/group_metadata.json',
     groupPolicyAsAdmin: true,
@@ -19,62 +20,133 @@ const msg = createGroupWithPolicy({
             metadata: 'First user'
         }
     ],
-    decisionPolicy: {
-        typeUrl: '/cosmos.group.v1.PercentageDecisionPolicy',
-        value: PercentageDecisionPolicy.encode({
-            percentage: '0.5',
-            windows: {
-                votingPeriod: {
-                    nanos: 0,
-                    seconds: Long.fromValue(100)
-                },
-                minExecutionPeriod: {
-                    nanos: 0,
-                    seconds: Long.fromValue(100)
-                }
+    decisionPolicy: PercentageDecisionPolicy.toProtoMsg({
+        percentage: '0.5',
+        windows: {
+            votingPeriod: {
+                nanos: 0,
+                seconds: Long.fromValue(100)
+            },
+            minExecutionPeriod: {
+                nanos: 0,
+                seconds: Long.fromValue(100)
             }
-        }).finish()
-    }
+        }
+    })
 });
 
 
-it('toAmino', () => {
-    const amino = MsgCreateGroupWithPolicy.toAmino(msg.value);
-    expect(amino).toMatchSnapshot();
-});
-
-interface Intf {
-    amino: any;
-    orig: any;
-    amino2: any;
-}
-const obj: Intf = {
-    //
+const policy: MsgCreateGroupWithPolicyEncoded = {
+    admin: 'address',
+    groupMetadata: 'https://some.ipfs.w3s.link/group_metadata.json',
+    groupPolicyAsAdmin: true,
+    groupPolicyMetadata: 'https://some.ipfs.w3s.link/group_metadata.json',
+    members: [
+        {
+            address: 'address',
+            weight: '10',
+            metadata: 'First user'
+        }
+    ],
+    decisionPolicy: PercentageDecisionPolicy.toProtoMsg({
+        percentage: '0.5',
+        windows: {
+            votingPeriod: {
+                nanos: 0,
+                seconds: Long.fromValue(100)
+            },
+            minExecutionPeriod: {
+                nanos: 0,
+                seconds: Long.fromValue(100)
+            }
+        }
+    })
 };
 
-it('fromAmino 1', () => {
-    const amino = obj.amino = MsgCreateGroupWithPolicy.toAmino(msg.value)
-    expect(obj.amino).toMatchSnapshot();
+// const policyMsg: MsgCreateGroupWithPolicyProtoMsg = MsgCreateGroupWithPolicy.toProtoMsg(msg)
+const protoMsg: MsgCreateGroupWithPolicyProtoMsg = MsgCreateGroupWithPolicy.toProtoMsg(policy)
+const msg = {
+    typeUrl: '/cosmos.group.v1.MsgCreateGroupWithPolicy',
+    value: policy
+}
 
-    expect(obj.amino.decision_policy).toMatchSnapshot();
-    const subtlediff = PercentageDecisionPolicy.fromAmino(obj.amino.decision_policy.value);
-    expect(subtlediff).toMatchSnapshot();
-    const encodedVersion = DecisionPolicy_FromAmino(obj.amino.decision_policy);
+
+
+// const msg = createGroupWithPolicy({
+//     admin: 'address',
+//     groupMetadata: 'https://some.ipfs.w3s.link/group_metadata.json',
+//     groupPolicyAsAdmin: true,
+//     groupPolicyMetadata: 'https://some.ipfs.w3s.link/group_metadata.json',
+//     members: [
+//         {
+//             address: 'address',
+//             weight: '10',
+//             metadata: 'First user'
+//         }
+//     ],
+//     decisionPolicy: {
+//         typeUrl: '/cosmos.group.v1.PercentageDecisionPolicy',
+//         value: PercentageDecisionPolicy.encode({
+//             percentage: '0.5',
+//             windows: {
+//                 votingPeriod: {
+//                     nanos: 0,
+//                     seconds: Long.fromValue(100)
+//                 },
+//                 minExecutionPeriod: {
+//                     nanos: 0,
+//                     seconds: Long.fromValue(100)
+//                 }
+//             }
+//         }).finish()
+//     }
+// });
+
+
+it('where it breaks', () => {
+    const myObj = MsgCreateGroupWithPolicy.toAmino(msg.value)
+    // myObj is correct
+    expect(myObj).toMatchSnapshot();
+
+    const encodedVersion = DecisionPolicy_FromAmino(myObj.decision_policy);
     expect({ encodedVersion }).toMatchSnapshot();
     const decodedVersion = DecisionPolicy_ToAmino(encodedVersion);
     expect({ decodedVersion }).toMatchSnapshot();
-    const simple1 = PercentageDecisionPolicy.fromAmino(obj.amino.decision_policy.value);
+
+
+    const encodedVersion2 = DecisionPolicy_FromAmino(decodedVersion);
+    expect({ encodedVersion2 }).toMatchSnapshot();
+    const decodedVersion2 = DecisionPolicy_ToAmino(encodedVersion2);
+    expect({ decodedVersion2 }).toMatchSnapshot();
+
+
+
+});
+
+it('tests', () => {
+    const objAmino = MsgCreateGroupWithPolicy.toAmino(msg.value)
+    expect(objAmino).toMatchSnapshot();
+    expect(objAmino.decision_policy).toMatchSnapshot();
+
+    const encodedVersion = DecisionPolicy_FromAmino(objAmino.decision_policy);
+    expect({ encodedVersion }).toMatchSnapshot();
+
+    const decodedVersion = DecisionPolicy_ToAmino(encodedVersion);
+    expect({ decodedVersion }).toMatchSnapshot();
+
+    const simple1 = PercentageDecisionPolicy.fromAmino(objAmino.decision_policy.value);
     expect(simple1).toMatchSnapshot();
+
     const simple2 = PercentageDecisionPolicy.toAmino(simple1);
     expect(simple2).toMatchSnapshot();
 
-    const origAttempt = obj.orig = MsgCreateGroupWithPolicy.fromAmino(obj.amino);
-    expect(obj.orig).toMatchSnapshot();
+    const origAttempt = MsgCreateGroupWithPolicy.fromAmino(objAmino);
+    expect(origAttempt).toMatchSnapshot();
 
-    console.log({ origAttempt })
+    // console.log({ origAttempt })
 
-    const aminoFromOrig = obj.amino2 = MsgCreateGroupWithPolicy.toAmino(obj.orig);
-    console.log(JSON.stringify({ aminoFromOrig }, null, 2))
-    console.log(JSON.stringify({ amino }, null, 2))
-    expect(obj.amino2).toEqual(obj.amino);
+    const aminoFromOrig = MsgCreateGroupWithPolicy.toAmino(origAttempt);
+    // console.log(JSON.stringify({ aminoFromOrig }, null, 2))
+    // console.log(JSON.stringify({ objAmino }, null, 2))
+    expect(aminoFromOrig).toEqual(objAmino);
 });
